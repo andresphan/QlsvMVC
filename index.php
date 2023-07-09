4003173c9a2f2a99d981500ff23062bafdd52c81
andresphan <loc.phan2113971@hcmut.edu.vn>
2023-07-09 23:25:52 +0700

    verson 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+session_start();
+//load models
+require "config.php";
+require "connectDB.php";
+require "functions.php";
+require "model/Student.php";
+require "model/StudentRepository.php";
+
+require "model/Subject.php";
+require "model/SubjectRepository.php";
+
+require "model/Register.php";
+require "model/RegisterRepository.php";
+
+//router
+$c = $_GET["c"] ?? "student";
+$a = $_GET["a"] ?? "list";
+$controllerName = ucfirst($c). "Controller";//StudentController
+require "controller/" . $controllerName . ".php";
+$controller = new $controllerName();//new StudentController();
+$controller->$a();//$controller->list();
+?>
---
+++ layout/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Quản lý sinh viên</title>
+		<link rel="stylesheet" href="public/vendor/bootstrap-4.5.3-dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="public/vendor/fontawesome-free-5.15.1-web/css/all.min.css">
+		<link rel="stylesheet" href="public/css/style.css">
+	</head>
+	<body>
+		<?php
+		global $c;
+		?>
+		<div class="container" style="margin-top:20px;">
+			<a href="/" class="<?=$c=='student' ? "active": ""?> btn btn-info">Students</a>
+			<a href="/?c=subject" class="<?=$c=='subject' ? "active": ""?> btn btn-info">Subject</a>
+			<a href="/?c=register" class="<?=$c=='register' ? "active": ""?> btn btn-info">Register</a>
+			<?php
+			$message = "";
+			if (!empty($_SESSION["success"])) {
+				$message = $_SESSION["success"];
+				$messageClass = "alert-success";
+				// xóa phần tử dựa vào key
+				unset($_SESSION["success"]);
+			}
+			else if (!empty($_SESSION["error"])) {
+				$message = $_SESSION["error"];
+				$messageClass = "alert-danger";
+				// xóa phần tử dựa vào key
+				unset($_SESSION["error"]);
+			}
+			
+			?>
+			<?php if ($message) { ?>
+			<div class="alert <?=$messageClass?> mt-4">
+				<?=$message?>
+			</div>
+			<?php } ?>
